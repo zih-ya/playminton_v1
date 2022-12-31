34238eb2ece4dad7d828fea24617de68958dd6df
Tzu-Ya Chao <zihyachao@gmail.com>
2022-12-31 18:43:16 +0800

    deploy setting

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,13 +15,13 @@ app.use(express.json());
 app.use("/", routes);
 db.connect();
 
-// if (process.env.NODE_ENV === "production") {
-//   const __dirname = path.resolve();
-//   app.use(express.static(path.join(__dirname, "../frontend", "build")));
-//   app.get("/*", function (req, res) {
-//     res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 4000;
 
