f23173ed3e97bc5f4da38b41785dd979b0ac1429
Tzu-Ya Chao <zihyachao@gmail.com>
2023-01-04 19:08:31 +0800

    fix api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// const API_ROOT =
-//   process.env.NODE_ENV === "production" ? "/" : "http://localhost:4000";
+const API_ROOT =
+  process.env.NODE_ENV === "production" ? "/" : "http://localhost:4000/";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: API_ROOT,
 });
 
 export default instance;
